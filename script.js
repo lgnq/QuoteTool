45cbb6c8e1016bf7074409eda58d69e39b433809
lgnq <eaf@melexis.com>
2026-01-22 08:06:57 +0800

    Implement service worker registration
    Add service worker registration on page load

--- script.js
+++ script.js
@@ -30,3 +30,17 @@ swap.addEventListener("click", () => {
 });
 
 calculate();
+
+if (navigator.serviceWorker) {
+    // Start registration process on every page load
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            // The register function takes as argument
+            // the file path to the worker's file
+            .register('./sw.js')
+            // Gives us registration object
+            .then(reg => console.log('Service Worker Registered'))
+            .catch(swErr => console.log(
+                  `Service Worker Installation Error: ${swErr}}`));
+      });
+}
